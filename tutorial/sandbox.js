6369cdcbb1922717b6e17d64717c1bbaa631d1ed
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-10 10:23:23 +0530

    watching netninja tutorial

---
+++ tutorial/sandbox.js
@@ -0,0 +1,7 @@
+var charecter = "mario11";
+console.log(charecter);
+var inputs = document.querySelectorAll("input");
+// console.log(inputs);
+inputs.forEach(function (element) {
+    console.log(element);
+});
---
+++ tutorial/sandbox.ts
@@ -0,0 +1,11 @@
+const charecter = "mario11";
+
+console.log(charecter);
+
+const inputs = document.querySelectorAll("input");
+
+// console.log(inputs);
+
+inputs.forEach((element) => {
+  console.log(element);
+});
