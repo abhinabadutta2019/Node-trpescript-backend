359a0dfcd7f08896b9474e45fa659287409bbb01
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-12 03:10:34 +0530

    trying zod

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,7 +1,27 @@
 import { Request, Response } from "express";
 import { Task } from "../models/Task";
 //
+//////////ZOD setup starts/////
+import { z } from "zod";
+import { fromZodError } from "zod-validation-error";
+//
+const TaskSchema = z.object({
+  title: z.string().min(5).default("null"),
+  completed: z.boolean().default(false),
+});
+//
+type OneTask = z.infer<typeof TaskSchema>;
+//
+const oneTask = { title: "a", completed: "play " };
+//
+try {
+  console.log(TaskSchema.parse(oneTask), "response from zod");
+} catch (err) {
+  const validationError = fromZodError(err as z.ZodError);
+  console.log(validationError);
+}
 
+/////////////
 //
 const createTask = async (req: Request, res: Response) => {
   try {
