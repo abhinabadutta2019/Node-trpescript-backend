b431a9222847c36bc19e1ae1a8c9f80084bee084
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-11 18:52:16 +0530

    setting up mongoDB connection in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json()); // Middleware to parse JSON requests
 dotenv.config();
+//////////////////////////////////////////
 
+///////mongoDB cloud//////////////////
+let uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.te788iv.mongodb.net/new-MERN-backend?retryWrites=true&w=majority`;
+//
+
+async function connectToMongoDB() {
+  try {
+    //if mongoDB uri is correct
+    //if it is connected
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    //if error in connection or - in mongoDB uri
+    console.error("MongoDB connection error:", error);
+  }
+}
+
+// Call the async function to connect to MongoDB
+connectToMongoDB();
+
+////////////////////////////////////////////
 console.log("Hi1");
 
 app.get("/", (req, res) => {
