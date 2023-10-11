80a46a8e6f7a1d5e08072acdc4641941e7aceb34
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-11 20:29:37 +0530

    added some routes to task.ts

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  status: { type: Boolean, required: true },
+  completed: { type: Boolean, required: true },
 });
 
 const Task = mongoose.model("task", taskSchema);
--- src/routers/task.ts
+++ src/routers/task.ts
@@ -5,4 +5,38 @@ import { Router } from "express";
 const router = Router();
 
 //
+//
+
+router.post("/", async (req, res) => {
+  try {
+    const task = new Task({
+      name: req.body.name,
+      completed: req.body.completed,
+    });
+    console.log(task);
+    //
+    const createdTask = await task.save();
+    res.json({ createdTask: createdTask });
+  } catch (err) {
+    res.json(err);
+  }
+});
+//
+router.get("/", async (req, res) => {
+  try {
+    const tasks = Task.find({});
+    console.log(tasks);
+
+    // if (task.length > 0) {
+    //   return res.json({ message: "no task found" });
+    // }
+    //
+
+    res.json({ tasks: tasks });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//
 export { router as tasksRouter };
