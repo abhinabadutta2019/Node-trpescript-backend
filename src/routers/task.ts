641183c3de04a8f32ae4e32b8cdd74f85e70b9d4
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-11 23:38:21 +0530

    test

--- src/routers/task.ts
+++ src/routers/task.ts
@@ -16,20 +16,6 @@ router.get("/", getTasks);
 router.put("/:id", updateTaskByID);
 router.delete("/:id", deleteTaskByID);
 //
-// router.delete("/:id", async (req, res) => {
-//   try {
-//     const task = await Task.findById(req.params.id);
 
-//     if (!task) {
-//       return res.json({ message: "Task not found" });
-//     }
-
-//     const deleteTask = await Task.findOneAndDelete({ _id: req.params.id });
-
-//     res.json({ deleteTask: deleteTask });
-//   } catch (err) {
-//     res.json(err);
-//   }
-// });
 //
 export { router as tasksRouter };
