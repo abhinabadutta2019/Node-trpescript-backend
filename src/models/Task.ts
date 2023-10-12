1c78edeaa1c1503d9cc3033cad3d4c606ed7ec9f
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-13 00:14:14 +0530

    added  some key  to mongoose schema

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
-const taskSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  completed: { type: Boolean, required: true },
-});
+const taskSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    completed: { type: Boolean, required: true },
+    description: { type: String },
+    slot: { type: String },
+  },
+  { timestamps: true }
+);
 
 const Task = mongoose.model("task", taskSchema);
 export { Task };
