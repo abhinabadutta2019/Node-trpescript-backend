6c70b7633ab88a08ce284687baa22275d9282e1a
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-16 15:26:38 +0530

    made changes

--- src/middleware/requireAuth.ts
+++ src/middleware/requireAuth.ts
@@ -1,18 +1,21 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { User } from "../models/User";
 import { Request, Response, NextFunction } from "express";
 
 const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
-  const { authoriztion } = req.headers;
+  const { authorization } = req.headers;
 
-  if (!authoriztion) {
+  if (!authorization) {
     return res.status(401).json({ error: "authorization token required" });
   }
 
-  const token = authoriztion.split(" ")[1];
+  const token = authorization.split(" ")[1];
   //
   try {
-    const { _id } = jwt.verify(token, process.env.JWT_SECRET as string);
+    const { _id } = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as JwtPayload;
     //this set to only send id - of the user - from all his details
     req.user = await User.findOne({ _id }).select("_id");
     next();
